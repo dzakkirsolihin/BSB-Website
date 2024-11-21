ba1b9d69a8d30ed8301c6ab9767fe0612f086a50
AkfalSY <akfaltara@upi.edu>
2024-11-21 07:21:19 +0700

    login

--- public/index.php
+++ public/index.php
@@ -1,17 +1,49 @@
-<?php
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-use Illuminate\Http\Request;
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Log In</title>
+  </head>
+  <body>
+    <!-- main container -->
+    <div class="container d-flex justify-content-center align-items-center min-vh-100">
 
-define('LARAVEL_START', microtime(true));
-
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
-
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+    <!-- login container -->
+        <div class="row border rounded-4 shadow box-area" style=" background-image: url('img/login.png');  border-radius: 50px !important;">
+    <!-- left container -->
+            <div class="col-md-6 d-flex justify-content-center align-items-center left-box" style="background: #F2F6FF; border-radius: 47px !important;">
+                <div class="rounded-4 box1 mb-5" style="border-radius: 25px !important;">  
+                    <label for="exampleInputEmail1" class="d-flex justify-content-center mb-3 fs-4" style="background: linear-gradient(to left,#00CE74,#019796); -webkit-background-clip: text; -webkit-text-fill-color:transparent; font-weight:600; padding-top: -80px;">Log In</label>  
+                        <div class="row align-items-center bg-white rounded-4 box ms-0" style="border-radius: 25px !important;">
+                                <div class="mb-3 mt-4">
+                                    <label for="exampleInputEmail1" class="form-label ms-4" style="background: linear-gradient(to left,#00CE74,#019796); -webkit-background-clip: text; -webkit-text-fill-color:transparent;">Username</label>
+                                    <input type="email" class="form-control container d-flex justify-content-center" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Isi email disini">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleInputPassword1" class="form-label ms-4" style="background: linear-gradient(to left,#00CE74,#019796); -webkit-background-clip: text; -webkit-text-fill-color:transparent;">Password</label>
+                                    <input type="password" class="form-control container d-flex justify-content-center" id="exampleInputPassword1" aria-describedby="emailHelp" placeholder="********">
+                                </div>
+                                <div class="input-group mb-3">
+                                    <button class="btn btn-lg w-100 fs-4 text-white fs-2" style="background: linear-gradient(to left,#00CE74,#019796);">Log In</button>
+                                </div>
+                        </div>
+                </div>
+            </div>
+    <!-- right container -->
+            <div class="col-md-6 d-flex justify-content-center align-items-center flex-column right-box" style="background:linear-gradient(#00CE74,#019796); border-radius: 50px !important;">
+                <div class="featured-image mb-3">
+                    <img src="img/Ellipse 3.png" class="img-fluid" style="width: 250px;">
+                </div>
+                <p class="text-white fs-2" style="font-family: 'Courier New', Courier, monospace; font-weight:600;">Baitush Sholihin</p>
+                <small class="text-white text-wrap text-center" style="width: 17rem; font-family:'Courier New', Courier, monospace;">Copyright @ 2024, Last Minute</small>
+            </div>
+        </div>
+    </div>
+  </body>
+</html>
