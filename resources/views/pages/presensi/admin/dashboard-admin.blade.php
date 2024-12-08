719fe54dfe0470647a41be1428f6c62c775c1be8
D. RikaZ <119458243+dzakkirsolihin@users.noreply.github.com>
2024-12-09 00:42:51 +0700

    update fitur
    penambahan fitur role kelas di port admin page kelola guru

--- resources/views/pages/presensi/admin/dashboard-admin.blade.php
+++ resources/views/pages/presensi/admin/dashboard-admin.blade.php
@@ -6,7 +6,7 @@
             <div>
                 <div class="card h-100 shadow-sm rounded-4">
                     <img src="{{ asset('Assets/presensi/presensi-guru.jpg') }}" 
-                        class="card-img-top rounded-4 img-fluid" alt="Presensi Guru" style="height: 300px; object-fit: cover;">
+                        class="card-img-top rounded-top-4 img-fluid" alt="Presensi Guru" style="height: 300px; object-fit: cover;">
                     <div class="card-body text-center">
                         <h3 class="card-title text-primary-custom">KELOLA AKUN GURU</h3>
                         <p class="card-text text-primary-custom">Daftarkan atau hapus akun guru</p>
@@ -19,7 +19,7 @@
             <div>
                 <div class="card h-100 shadow-sm rounded-4">
                     <img src="{{ asset('Assets/presensi/presensi-murid.jpg') }}" 
-                        class="card-img-top rounded-4 img-fluid" alt="Riwayat Presensi" style="height: 300px; object-fit: cover;">
+                        class="card-img-top rounded-top-4 img-fluid" alt="Riwayat Presensi" style="height: 300px; object-fit: cover;">
                     <div class="card-body text-center">
                         <h3 class="card-title text-primary-custom">KELOLA KELAS</h3>
                         <p class="card-text text-primary-custom">Tambahkan atau hapus daftar murid</p>
@@ -32,7 +32,7 @@
             <div>
                 <div class="card h-100 shadow-sm rounded-4">
                     <img src="{{ asset('Assets/presensi/riwayat-presensi.jpg') }}" 
-                        class="card-img-top rounded-4 img-fluid" alt="Presensi Murid" style="height: 300px; object-fit: cover;">
+                        class="card-img-top rounded-top-4 img-fluid" alt="Presensi Murid" style="height: 300px; object-fit: cover;">
                     <div class="card-body text-center">
                         <h3 class="card-title text-primary-custom">LAPORAN PRESENSI</h3>
                         <p class="card-text text-primary-custom">Periksa dan download laporan presensi</p>
--- resources/views/pages/presensi/admin/kelola-kelas.blade.php
+++ resources/views/pages/presensi/admin/kelola-kelas.blade.php
@@ -2,59 +2,66 @@
     <main class="container py-4">
         <h1 class="text-center inter-font text-primary-custom mb-5">Kelola Murid dalam Kelas</h1>
         
-        <div class="row g-4 justify-content-center">
-            <a href="kelola-kelas-daycare" class="col-md-3 text-decoration-none">
-                <div>
+        <div class="row row-cols-1 row-cols-md-4 g-4 justify-content-center">
+            <div class="col">
+                <a href="kelola-kelas-daycare" class="text-decoration-none">
                     <div class="card h-100 shadow-sm rounded-4">
                         <img src="{{ asset('Assets/presensi/daycare.jpg') }}" 
-                            class="card-img-top rounded-4" alt="foto murid Daycare" style="height: 200px; object-fit: cover;">
+                            class="card-img-top rounded-top-4" 
+                            alt="foto murid Daycare" 
+                            style="height: 200px; object-fit: cover;">
                         <div class="card-body text-center">
                             <h3 class="card-title text-primary-custom">DAYCARE</h3>
                             <p class="card-text text-primary-custom">Cek dan catat kehadiran guru</p>
                         </div>
                     </div>
-                </div>
-            </a>
+                </a>
+            </div>
 
-            <a href="kelola-kelas-tk-a" class="col-md-3 text-decoration-none">
-                <div>
+            <div class="col">
+                <a href="kelola-kelas-tk-a" class="text-decoration-none">
                     <div class="card h-100 shadow-sm rounded-4">
                         <img src="{{ asset('Assets/presensi/tk-a.jpg') }}"
-                            class="card-img-top rounded-4" alt="foto murid TK A" style="height: 200px; object-fit: cover;">
-                        <div class="card-body text-center"></div>
+                            class="card-img-top rounded-top-4" 
+                            alt="foto murid TK A" 
+                            style="height: 200px; object-fit: cover;">
+                        <div class="card-body text-center">
                             <h3 class="card-title text-primary-custom">TK A</h3>
                             <p class="card-text text-primary-custom">Cek dan catat kehadiran guru</p>
                         </div>
                     </div>
-                </div>
-            </a>
+                </a>
+            </div>
             
-            <a href="kelola-kelas-tk-b" class="col-md-3 text-decoration-none">
-                <div>
+            <div class="col">
+                <a href="kelola-kelas-tk-b" class="text-decoration-none">
                     <div class="card h-100 shadow-sm rounded-4">
                         <img src="{{ asset('Assets/presensi/tk-b.jpg') }}" 
-                            class="card-img-top rounded-4" alt="foto murid TK B" style="height: 200px; object-fit: cover;">
+                            class="card-img-top rounded-top-4" 
+                            alt="foto murid TK B" 
+                            style="height: 200px; object-fit: cover;">
                         <div class="card-body text-center">
                             <h3 class="card-title text-primary-custom">TK B</h3>
                             <p class="card-text text-primary-custom">Lihat rekam jejak kehadiran</p>
                         </div>
                     </div>
-                </div>
-            </a>
+                </a>
+            </div>
             
-            <a href="kelola-kelas-bestari" class="col-md-3 text-decoration-none">
-                <div>
+            <div class="col">
+                <a href="kelola-kelas-bestari" class="text-decoration-none">
                     <div class="card h-100 shadow-sm rounded-4">
                         <img src="{{ asset('Assets/presensi/bestari.jpg') }}" 
-                            class="card-img-top rounded-4" alt="foto murid Bestari" style="height: 200px; object-fit: cover;">
+                            class="card-img-top rounded-top-4" 
+                            alt="foto murid Bestari" 
+                            style="height: 200px; object-fit: cover;">
                         <div class="card-body text-center">
                             <h3 class="card-title text-primary-custom">BESTARI</h3>
                             <p class="card-text text-primary-custom">Pantau absensi siswa harian</p>
                         </div>
                     </div>
-                </div>
-            </a>
-            
+                </a>
+            </div>
         </div>
     </main>
 </x-layout-admin>
